e7c336e2aca0e8d87d6d7dd6a8f526f5dc35de29
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2024-11-20 13:23:38 -0300

    refactor: simplify simulation in class Main

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import Observer.Television;
 import Subject.RiskCategory;
 import Subject.RiskNotifier;
 import Observer.Smartphone;
-
 import java.util.Random;
 
 public class Main {
@@ -10,30 +9,14 @@ public class Main {
         RiskNotifier app = RiskNotifier.getInstance();
         RiskCategory[] riskLevels = RiskCategory.values();
         Random random = new Random();
-        Smartphone smartphone1 = new Smartphone("g");
-        Smartphone smartphone2 = new Smartphone("a");
+        Smartphone smartphone1 = new Smartphone("John");
         Television television1 = new Television();
-
         app.addSubscriber(smartphone1);
-        app.addSubscriber(smartphone2);
         app.addSubscriber(television1);
-
-
-        for (int i = 0; i < 6; i++) {
-            if(i == 3) app.removeSubscriber(smartphone2);
-            if(i == 5) app.addSubscriber(smartphone2);
-            RiskCategory newRiskLevel = riskLevels[random.nextInt(riskLevels.length)];
-            app.updateRiskLevel(newRiskLevel);
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                System.err.println("Error executing thread: " + e.getMessage());
-                Thread.currentThread().interrupt();
-            }
-        }
-
-        app.removeSubscriber(smartphone1);
-        app.removeSubscriber(smartphone2);
-        System.out.println("\nEnd of simulation.");
+        RiskCategory riskLevel1 = riskLevels[random.nextInt(riskLevels.length)];
+        app.updateRiskLevel(riskLevel1);
+        app.removeSubscriber(television1);
+        RiskCategory riskLevel2 = riskLevels[random.nextInt(riskLevels.length)];
+        app.updateRiskLevel(riskLevel2);
     }
 }
